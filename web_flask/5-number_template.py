b94429d428f12c19e7b7ae97eb36214585e83443
ObedGreatness <obedyawson155@gmail.com>
2024-03-23 19:59:14 -0700

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,5 +44,5 @@ def num_html(n):
     return render_template('5-number.html', name=n)
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
